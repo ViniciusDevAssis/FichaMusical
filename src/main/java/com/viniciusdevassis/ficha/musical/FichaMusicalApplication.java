45f13b3f5822bc0ea7c36c8aadbf41f2731873a4
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2026-01-30 12:15:27 -0300

    InIInício do projeto

---
+++ src/main/java/com/viniciusdevassis/ficha/musical/FichaMusicalApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusdevassis.ficha.musical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichaMusicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichaMusicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusdevassis/ficha/musical/FichaMusicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusdevassis.ficha.musical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaMusicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
